59520955af463b35cc45adc2f45160a7894cf720
esmaeel67 <esmaeel.babaee@gmail.com>
2026-08-29 13:50:30 +0330

    feat(posting_service_test): implemented newOrder method test

---
+++ db/posting_service_test.go
@@ -0,0 +1,21 @@
+package db_test
+
+import (
+	"testing"
+
+	"github.com/esmaeel67/golang-tdd.git/db"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewOrder(t *testing.T) {
+
+	ps := db.NewPostingService()
+	b := db.Book{
+		ID: uuid.New().String(),
+	}
+
+	err := ps.NewOrder(b)
+
+	assert.Nil(t, err)
+}
